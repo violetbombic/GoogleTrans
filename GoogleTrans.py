4e6e8b4439e135bb6f84792dd26041fcd77bb8cb
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-25 12:11:01 +0200

    Update GoogleTrans.py

--- GoogleTrans.py
+++ GoogleTrans.py
@@ -3,12 +3,11 @@ from googletrans import Translator
 
 translator = Translator()
 
-while True:
-  word = st.text_input("Give me a word you want me to translate: ")
-  word_it = translator.translate(word, src = 'en', dest = 'it')
-  st.write(word_it.text)
-  st.write("Do you want me to continue? Write 'no' in case not.")
-  if word == 'no':
-    st.write("OK, Goodbye!")
-    break
+word = st.text_input("Give me a word you want me to translate: ")
+word_it = translator.translate(word, src = 'en', dest = 'it')
+st.write(word_it.text)
+st.write("Do you want me to continue? Write 'no' in case not.")
+if word == 'no':
+  st.write("OK, Goodbye!")
+  break
 
