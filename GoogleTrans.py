6bd04c380d5483267de6d6c318bf914aaf4dbab8
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-25 11:16:41 +0200

    Update GoogleTrans.py

--- GoogleTrans.py
+++ GoogleTrans.py
@@ -1,13 +1,14 @@
+import streamlit as st 
 from googletrans import Translator
 
 translator = Translator()
 
 while True:
-  word = input("Give me a word you want me to translate: ")
+  word = st.input_text("Give me a word you want me to translate: ")
   word_it = translator.translate(word, src = 'en', dest = 'it')
-  print(word_it.text)
-  print("Do you want me to continue? Write 'no' in case not.")
+  st.write(word_it.text)
+  st.write("Do you want me to continue? Write 'no' in case not.")
   if word == 'no':
-    print("OK, Goodbye!")
+    st.write("OK, Goodbye!")
     break
 
