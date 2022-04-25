51a80a2f2679b19e9c28549b37fdbc9071e68b05
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-25 13:25:14 +0200

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -5,6 +5,7 @@ translator = Translator()
 
 word = st.text_input("Give me a word you want me to translate: ")
 target_l = st.radio("Give me a target language", ("it", "en", "sk", "de")).lower()
+target_select = st.selectbox("Give me a target language", ("it", "en", "sk")).lower()
 
 
 word_it = translator.translate(word, dest = target_l)
