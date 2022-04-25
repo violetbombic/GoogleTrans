0f13f74065a71b10111b7e2ebf83bfb6076047f4
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-25 13:17:55 +0200

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+from googletrans import Translator
+
+translator = Translator()
+
+word = st.text_input("Give me a word you want me to translate: ")
+target_l = st.radio("Give me a target language (it, en, sk...): ").lower()
+
+
+word_it = translator.translate(word, dest = target_l)
+st.write(word_it.text)
+st.write("Do you want me to continue? Write 'no' in case not.")
+if word == 'no':
+  st.write("OK, Goodbye!")
